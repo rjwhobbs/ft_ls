bbb419572d70362bc327ee631fd20f9f939ca855
Roger HOBBS <rhobbs@c4r5s3.wethinkcode.co.za>
2019-07-10 12:10:51 +0200

    A neat little trick using bitwise op &

--- main.c
+++ main.c
@@ -1,55 +1,59 @@
 #include "libft/libft.h"
 
-int isflag(int c)
+int isflag(char c)
 {
-	if (c == 'a' || c == 'l' || c == 'r' || c == 'R' || c == 't')
+	if (c == 'a' || c == 'l' || c == 'r' || c == 't' || c == 'R')
 		return (1);
 	return (0);
 }
 
-int process_args(int ac, char **av, char **flags)
+int process_flags(char **av, char *op)
 {
-	int i = 1;
-	int j = 0;
+	int i;
+	int j;
 
-	*flags = ft_strnew(5);
-	while (i < ac)
+	i = 0;
+	j = 0;
+	if (av[i][j] == '-')
+		j++;
+	while ((av[i][j]) && isflag(av[i][j]))
 	{
-		if(av[i][j++] == '-')
-		{
-			while (av[i][j])
-			{
-				if (isflag(av[i][j]))
-					ft_strncat(*flags, av[i] + j++, 1);
-				else
-					return (-1);
-			}
-		}
-		else
-			break;
-		j = 0;
-		i++;
+		if (av[i][j] == 'a' && !(*op & 1))
+			*op += 1;
+		if (av[i][j] == 'l' && !(*op & 2))
+			*op += 2;
+		if (av[i][j] == 'r' && !(*op & 4))
+			*op += 4;
+		if (av[i][j] == 't' && !(*op & 8))
+			*op += 8;
+		if (av[i][j] == 'R' && !(*op & 16))
+			*op += 16;
+		j++;
 	}
-	return (i);
+	if (!(av[i][j]))
+		return (1);
+	return (0);
 }
 
 int main(int ac, char **av)
 {
 	char *flags;
 	char **dirs;
+	char op;
+	int i;
 
-	if (ac > 1)
-	process_args(ac, av, &flags);
+	op = 0;
+	i = 1;
 
-	while (*flags)
-	{
-		ft_putchar(*flags++);
-		ft_nl();
-	}
-	/*
-	while(*dirs)
-		ft_putendl(*dirs++);
-	*/
-	//ft_strdel(&flags);
+
+	if (av[1][0] == '-')
+		while (i < ac)
+		{
+			process_flags(&av[i], &op);
+			i++;
+		}
+
+	ft_putnbr(op);
+
 	return (0);
 }
