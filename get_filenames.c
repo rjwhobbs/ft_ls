291fe1710c2d237638f8c2713e9bcac1380b14b5
mmamalek <mmamalek@student.wethinkcode.co.za>
2019-07-15 05:41:55 +0200

    get_filenames is also modal. You tell it whether you want to include all files or just the files without a dot

--- get_filenames.c
+++ get_filenames.c
@@ -1,7 +1,7 @@
 #include <dirent.h>
 #include "ft_ls.h"
 
-char **get_filenames(char *dirname)
+static char **filenames_all(char *dirname)
 {
 	DIR				*dir;
 	int				n;
@@ -9,7 +9,7 @@ char **get_filenames(char *dirname)
 	char			**files;
 	char			**temp;
 
-	n = count_files(dirname);
+	n = count_files(dirname, 'a');
 	files = (char **)malloc(sizeof(char *) * (n + 1));
 	temp = files;
 	dir = opendir(dirname);
@@ -23,4 +23,38 @@ char **get_filenames(char *dirname)
 	closedir(dir);
 	files = temp;
 	return (files);
+}
+
+
+static char **filenames(char *dirname)
+{
+	DIR				*dir;
+	int				n;
+	struct dirent	*file;
+	char			**files;
+	char			**temp;
+
+	n = count_files(dirname, '-');
+	files = (char **)malloc(sizeof(char *) * (n + 1));
+	temp = files;
+	dir = opendir(dirname);
+	if (dir == NULL)
+		return (NULL);
+	while ((file = readdir(dir)) != NULL)
+	{
+		if (file->d_name[0] != '.')
+			*files++ = ft_strdup(file->d_name);
+	}
+	*files = NULL;
+	closedir(dir);
+	files = temp;
+	return (files);
+}
+
+char **get_filenames(char *dirname, int mode)
+{
+	if (mode == 'a')
+		return (filenames_all(dirname));
+	else
+		return (filenames(dirname));
 }
