106ec7ee55b9c19d0209446178eb1247f78566a0
mmamalek <mmamalek@student.wethinkcode.co.za>
2019-07-15 05:40:19 +0200

    count_files() is now modal. You pass an extra argument to it to tell if whether to count all files (including hidden dot file) or just visible files

--- count_files.c
+++ count_files.c
@@ -1,17 +1,23 @@
 #include <dirent.h>
 #include "ft_ls.h"
 
-int count_files(char *dirname)
+int count_files(char *dirname, int mode)
 {
 	DIR	*dir;
+	struct dirent *file;
 	int	n;
 
 	n = 0;
 	dir = opendir(dirname);
 	if (dir == NULL)
 		return -1;
-	while (readdir(dir))
-		n++;
+	if (mode == 'a')
+		while (readdir(dir))
+			n++;
+	else
+		while ((file = readdir(dir)) != NULL)
+			if (file->d_name[0] != '.')
+			n++;
 	closedir(dir);
 	return (n);
 }
