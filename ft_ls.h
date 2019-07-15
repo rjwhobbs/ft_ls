b76671ece288c1d9669a94be54a0c8071d738c48
mmamalek <mmamalek@student.wethinkcode.co.za>
2019-07-15 05:42:24 +0200

    .

--- ft_ls.h
+++ ft_ls.h
@@ -2,8 +2,10 @@
 # define FT_LS_H
 # include "libft/libft.h"
 int		process_args(char *av[], char *operations[], char **files[]);
-int		count_files(char *dirname);
-char	**get_filenames(char *dirname);
+int		count_files(char *dirname, int mode);
+char	**get_filenames(char *dirname, int mode);
 char	**get_filenames_d(char *dirname);
+char	**get_dirnames(char parentdir, int mode);
 void	sort_lex(char ***names, int number);
+void	sort(char ***names);
 #endif
