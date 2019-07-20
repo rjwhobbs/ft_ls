2316bff78442bef0ac7d2ed5fc99d277126bb686
Roger HOBBS <rhobbs@c4r6s4.wethinkcode.co.za>
2019-07-20 11:22:09 +0200

    Working on how to ls on a file

---
+++ test_validdir.c
@@ -0,0 +1,19 @@
+#include "libft/libft.h"
+#include <sys/stat.h>
+#include <dirent.h>
+
+int main(int ac, char *av[])
+{
+    DIR *dir;
+    struct dirent *file;
+
+    dir = opendir(av[1]);
+    if (dir == NULL)
+        ft_putendl("dir is null");
+    while ((file = readdir(dir)) != NULL) //This causes a segfault if passed a dir pointer
+    {                                     //that is NULL.
+        ft_putendl("This happened");
+    }
+    ft_nl();
+    return (0);
+}
