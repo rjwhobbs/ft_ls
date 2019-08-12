9686165eff82a5073ada0978a8f12fb3891b5ec8
Roger HOBBS <rhobbs@c4r6s4.wethinkcode.co.za>
2019-08-12 14:59:48 +0200

    Cleaning rec funcs

---
+++ rec_operations.c
@@ -0,0 +1,44 @@
+#include "ft_ls.h"
+
+void			rec_no_files_no_l(char *ops)
+{
+	print_rec("./", ops);
+}
+
+void			rec_files_no_l(char *ops, char **files)
+{
+	sort(&files, ops);
+	while (*files)
+	{
+		if (((*files)[ft_strlen(*files) - 1]) != '/')
+		{
+			print_name(*files++);
+			ft_nl();
+		}
+		else
+			print_rec(*files++, ops);
+	}
+}
+
+void			rec_no_files_l(char *ops)
+{
+	print_rec("./", ops);
+}
+
+void			rec_files_l(char *ops, char **files)
+{
+	sort(&files, ops);
+	while(*files)
+	{
+		if (((*files)[ft_strlen(*files) - 1]) != '/')
+		{
+			print_file_l(files);
+			(files)++;
+		}
+		else
+		{
+			print_rec(*files, ops);
+			(files)++;
+		}
+	}
+}
